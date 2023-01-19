5ec5c44072101c38318d767156be9c1599f6f35a
Mrical Singhal <mynameismric@gmail.com>
2023-01-19 15:34:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sidenav';
+  selectedIndex:number=1;
+  opened=true;
+  constructor(private _formBuilder: FormBuilder) { }
+  reminderTypeList=false;
+  stockRelated=false;
+  hideRequiredControl = new FormControl(false);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,78 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from "@angular/material/toolbar"
+import {MatSidenavModule} from "@angular/material/sidenav"
+import {MatIconModule} from "@angular/material/icon"
+import {MatListModule} from "@angular/material/list"
+import {MatButtonModule} from "@angular/material/button"
+import {MatAutocompleteModule} from "@angular/material/autocomplete"
+import {MatButtonToggleModule} from "@angular/material/button-toggle"
+import {MatCardModule} from '@angular/material/card'
+import {MatCheckboxModule} from '@angular/material/checkbox'
+import {MatChipsModule} from '@angular/material/chips'
+import {MatStepperModule} from '@angular/material/stepper'
+import {MatDatepickerModule} from '@angular/material/datepicker'
+import {MatDialogModule} from '@angular/material/dialog'
+import {MatExpansionModule} from '@angular/material/expansion'
+import {MatMenuModule} from '@angular/material/menu'
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'
+import {MatRadioModule} from '@angular/material/radio'
+import {MatSliderModule} from '@angular/material/slider'
+import {MatSelectModule} from '@angular/material/select'
+import {MatProgressBarModule} from '@angular/material/progress-bar'
+import {MatPaginatorModule} from '@angular/material/paginator'
+import {MatInputModule} from '@angular/material/input'
+import {MatTabsModule} from '@angular/material/tabs'
+import {MatSlideToggleModule} from '@angular/material/slide-toggle'
+import {MatSnackBarModule} from '@angular/material/snack-bar'
+import {MatSortModule} from '@angular/material/sort'
+import {MatTooltipModule} from '@angular/material/tooltip'
+import {MatTableModule} from '@angular/material/table'
+import {MatGridListModule} from '@angular/material/grid-list'
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    // MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    // MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
